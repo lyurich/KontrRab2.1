955107ee470107bff594ddc56c2d225a3b1e4ea7
Leonid <leonid0485@gmail.com>
2023-06-06 16:33:52 +0300

    Kontr

---
+++ view.py
@@ -0,0 +1,55 @@
+
+class View(object):
+
+    @staticmethod
+    def show_number_point_list(notes):
+        for note in notes:
+
+            print(note)
+
+
+    @staticmethod
+    def show_note(note):
+
+        print(note)
+
+
+    @staticmethod
+    def show_empty_list_message():
+
+        print('Cписок заметок пустой!')
+
+
+    @staticmethod
+    def display_note_id_not_exist(note_id):
+
+        print('Заметка с id: {} не найдена!'.format(note_id))
+
+
+    @staticmethod
+    def display_note_id_exist(note_id):
+
+        print('Заметка с id: {} уже есть!'.format(note_id))
+
+
+    @staticmethod
+    def display_note_stored():
+
+        print('Заметка успешно добавлена!')
+
+    @staticmethod
+    def display_note_updated(note_id):
+        print( 'Заметка с id:{} обновлена успешно!'
+              .format(note_id) )
+
+    @staticmethod
+    def display_note_deletion(note_id):
+        print('Удаление заметки с id: {} выполнено!'.format(note_id) )
+
+    @staticmethod
+    def display_all_notes_deletion():
+        print('Все заметки удалены!')
+
+
+def display_note_id_not_exist(search_id):
+    return search_id
